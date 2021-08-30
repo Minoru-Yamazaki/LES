84139e62ef266daa2ff0d37a43c19eede5f4d7cb
Minoru-Yamazaki <minoru.rhcp@gmail.com>
2021-08-30 20:00:35 -0300

    Adicionado construtor

--- src/main/java/br/com/ecommerceorquideas/warning/Aviso.java
+++ src/main/java/br/com/ecommerceorquideas/warning/Aviso.java
@@ -1,16 +1,22 @@
 package br.com.ecommerceorquideas.warning;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import lombok.Getter;
 import lombok.Setter;
 
-public class Aviso {
-
 @Getter
 @Setter
+public class Aviso {
+
 	private List<String> mensagens;
+
+	public Aviso() {}
+	public Aviso(String mensagem) {
+		mensagens = Arrays.asList(mensagem);
+	}
 	
 	public void addMensagem(String mensagem) {
 		if(mensagens == null) {
